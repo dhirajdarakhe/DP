4187e02ec834097c54f87377b1c95d9ea4a6b0a6
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2023-06-08 14:43:43 +0530

    42_43_44_Longest_Increasing _Subsequence
    R

--- 26_LIS.cpp
+++ 26_LIS.cpp
@@ -1,62 +1,90 @@
 // https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/submissions/ 
-// nlogn
-class Solution {
+class Solution
+{
 public:
-    vector<int> longestObstacleCourseAtEachPosition(vector<int>& nums) {
-        
-        int n = nums.size();
-        
-        
-        vector<int> lis;
-        
-        
-        vector<int> ans;
-
-        for(int i = 0; i < n; i++)
-        {
-            if(lis.size() == 0 || lis.back() <= nums[i])
-            {  
-              lis.push_back(nums[i]);
-              ans.push_back(lis.size();
-            }
-            else
-            {
-                int idx = upper_bound(lis.begin(), lis.end(), nums[i]) - lis.begin();
-                ans.push_back(idx + 1);
-                lis[idx] = nums[i];
-            }
-        }
-        
-        return ans;
-    }
-};
-
-                            
-                             int f(vector<int> &nums, int i, int pre,vector<vector<int>> &dp)
+    int f(vector<int> &nums, int i, int pre, vector<vector<int>> &dp)
     {
-        if(i >= nums.size()) return 0;
-        if(dp[i][pre+1] != -1) return dp[i][pre+1];
-        int ntk = f(nums, i+1, pre, dp);
+        if (i >= nums.size())
+            return 0;
+        if (dp[i][pre + 1] != -1)
+            return dp[i][pre + 1];
+        int ntk = f(nums, i + 1, pre, dp);
         int tk = INT_MIN;
-        if(pre == -1 || nums[i] > nums[pre])
+        if (pre == -1 || nums[i] > nums[pre])
         {
             tk = 1 + f(nums, i + 1, i, dp);
         }
-        return dp[i][pre+1] = max(tk, ntk);
+        return dp[i][pre + 1] = max(tk, ntk);
     }
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        
+        // r3ecursion
+        // o(n*n) time;
+        // o(n*n) + o(n) space
+        // vector<vector<int>> dp(nums.size()+10, vector<int>(nums.size()+10, -1));
+        // return f(nums, 0,-1, dp);
+
+        // o(n*n) time;
+        // o(n*n) space
+        // tabulation
+        // not-understood
+
+        
+        
+        
+        
+        
+        
+        
+        
+        // o(n*n) time
+        // o(n) space
+        vector<int> dx(n, 1);
+        vector<int> print(n);
+        int mx = -1;
+        int idx = -1;
+        for (int i = 1; i < n; ++i)
+        {  
+            print[i] = i;
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[i] > nums[j] && dx[i] < dx[j] + 1)
+                {
+                    dx[i] = dx[j] + 1;
+                    print[i] = j;
+                }
+            }
+            if(mx < dx[i])
+            {
+                mx = dx[i];
+                idx = i;
+            }
+        }
+        // printing
+        cout<<nums[idx]<<" ";
+        while(print[idx] != idx)
+        {
+            cout<<nums[print[idx]]<<" ";
+            idx = print[idx]; 
+        }
+        return mx;
+
+        
+        
+        
+        // binary serach problm
+        // o(nlon(n)) time
+        // o(n) space
+        // vector<int> lis;
+        // lis.push_back(nums[0]);
+        // for(int i = 1; i < n; i++)
+        // {
+        //         if(lis.back() < nums[i]) lis.push_back(nums[i]);
+        //         int idx = lower_bound(lis.begin(), lis.end(), nums[i]) - lis.begin();
+        //         lis[idx] = nums[i];
+        // }
+        // return lis.size();
+    }
+};
